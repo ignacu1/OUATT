58ea3390a0704a2d53db32f9fee320663cc3398b
Maurycc <maury.brzoskowski@gmail.com>
2021-01-05 15:58:49 +0100

    ScrollingBG

---
+++ OUATTUnity/Assets/scrollingBackGroundScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingBackGroundScript : MonoBehaviour
+{
+
+    public bool ResetedBackground1 = false;
+    public Transform point1;
+    public Transform point2;
+    public Transform BG1;
+    public Transform BG2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, point1.position) < 5 && ResetedBackground1 == false){
+            ResetBackground1();
+        }
+
+        if(Vector3.Distance(transform.position, point2.position) < 15 && ResetedBackground1 == true){
+            ResetBackground2();
+        }
+    }
+
+    void ResetBackground1(){
+        BG2.position = new Vector3(BG1.position.x + 24.5f, BG2.position.y, BG2.position.z);
+        ResetedBackground1 = true;
+    }
+
+    void ResetBackground2(){
+        BG1.position = new Vector3(BG2.position.x + 24.5f, BG1.position.y, BG1.position.z);
+        ResetedBackground1 = false;
+    }   
+}
